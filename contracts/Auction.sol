028d9a83de59f663c9edaebc49c07d3073f286ca
akshatcx <akshat.c2k@gmail.com>
2021-10-11 15:36:09 +0530

    add: factory and SPBSA auction

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -1,23 +1,48 @@
 pragma solidity >=0.7.0 <0.9.0;
 
-contract Auction {
+/* Auction strats:
+    0: First Price Sealed-Bid Auction
+    1: Second Price Sealed-Bid Auction
+    2: Average Price Sealed-Bid Auction
+*/
+
+contract Factory{
+    Auction[] public auctions;
+
+    event AuctionCreated(address autionAddress, string name, uint _strat);
+    
+    function createAuction(string memory _name, uint _basePrice, uint _bidDuration, uint _revealDuration, uint _strat) external {
+       Auction auction = new Auction(auctions.length, _name, _basePrice, _bidDuration, _revealDuration, _strat);
+       auctions.push(auction);
+       emit AuctionCreated(address(auction), _name, _strat);
+     }
+}
+
+contract Auction { 
+    uint public index;
+    
     address payable seller;
+    uint strat;
     string public name;
-    uint256 public basePrice;
-    uint256 public bidEnd;
-    uint256 public revealEnd;
-    uint256 public maxBid;
+    uint public basePrice;
+    uint public bidEnd;
+    uint public revealEnd;
+    uint public maxBid;
+    uint public secmaxBid;
+    uint public winAmount;
     address public winner;
 
 
-    mapping(address => uint256) public balances;
+    mapping(address => uint) public balances;
     mapping(address => bytes32) public hashedBids;
     
     event ClaimInitiated(string _public_key);
     event ItemTransferred(string _encrypted_item);
     
-    constructor (string memory _name, uint256 _basePrice, uint256 _bidDuration, uint256 _revealDuration ) {
+    constructor (uint _index, string memory _name, uint _basePrice, uint _bidDuration, uint _revealDuration, uint _strat) {
+        index = _index;
         name = _name;
+        strat = _strat;
         basePrice = _basePrice;
         bidEnd = block.timestamp + _bidDuration;
         revealEnd = bidEnd + _revealDuration;
@@ -33,24 +58,40 @@ contract Auction {
         balances[msg.sender] += msg.value;
     }
     
-    function reveal(uint256 amount) public {
+    function reveal(uint amount) public {
         require(block.timestamp >= bidEnd, "Revealing period is yet to start");
         require(block.timestamp < revealEnd, "Revealing period has expired");
         require(keccak256(abi.encodePacked(amount)) == hashedBids[msg.sender]); //sha3
         require(amount >= basePrice, "Bid should not be less than base price");
         require(amount <= balances[msg.sender], "Bid less than deposit amount");
-
-        if (amount > maxBid) {
-            balances[winner] += maxBid;
-            maxBid = amount;
-            winner = msg.sender;
-            balances[winner] -= amount;
+    
+        if(strat == 0){
+            if(amount > maxBid){
+                balances[winner] += maxBid;
+                maxBid = amount;
+                winAmount = amount;
+                winner = msg.sender;
+                balances[winner] -= amount;
+            }
+        }
+        else if(strat == 1){
+            balances[winner] += winAmount;
+            if(amount >= maxBid){
+                secmaxBid = maxBid;
+                maxBid = amount;
+                winner = msg.sender;
+            }
+            if(amount >= secmaxBid){
+                secmaxBid = amount;
+            }
+            winAmount = secmaxBid;
+            balances[winner] -= winAmount;
         }
     }
     
     function withdraw() public {
         require(block.timestamp >= revealEnd, "Revealing period is yet to expire");
-        uint256 amount = balances[msg.sender];
+        uint amount = balances[msg.sender];
         balances[msg.sender] = 0;
         payable(msg.sender).transfer(amount);
     }
@@ -64,7 +105,8 @@ contract Auction {
     function transferItem(string memory _encrypted_item) public {
         require(block.timestamp >= revealEnd, "Revealing period is yet to expire");
         require(msg.sender == seller, "Only callable by seller");
-        seller.transfer(maxBid);
+        seller.transfer(winAmount);
         emit ItemTransferred(_encrypted_item);
     }
 }
+
